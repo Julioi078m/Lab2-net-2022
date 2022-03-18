dfcaa0d97efb20f81f21b40e1fb9c9c2c18823f7
Julioi078m <101900539+Julioi078m@users.noreply.github.com>
2022-03-18 15:10:53 -0600

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using Laboratorio2.Entities;
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+         Console.WriteLine("prueba");
+         Alumno alumno = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno1 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno2 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno3 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno4 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno5 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno6 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+         Alumno alumno7 = new Alumno("1234 123casd123", "15", new Persona("123asdf", "test", "test", "test@mail.com"));
+
+        alumno.tomarAsistencia(1);
+
+        Console.WriteLine("alumno 1 asistencia "+ alumno.asistencia + " "+ alumno.CUI);
+        
+    }
+}
